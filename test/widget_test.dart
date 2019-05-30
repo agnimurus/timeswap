9cef5301b7de1dbc53ae867d972042d87ae8d488
Pujit Mehrotra <pujitm2009@gmail.com>
2019-05-30 18:32:20 -0400

    Remove boilerplate test and Add login config test
    To debug issue #1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,18 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:timeswap/main.dart';
 
 void main() {
+
+  test('login config test', () async {
+    String config = await rootBundle.loadString('config/login/animation.json');
+    assert(config != null && config.isNotEmpty);
+  });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // TODO Implement
   });
 }
