070bf4e19752f63da97ba8066d215a8fb8063a15
Pujit Mehrotra <pujitm2009@gmail.com>
2019-05-31 18:34:13 -0400

    Fix #1
    Move config loading to build method via a `FutureBuilder`
    
    The asset bundle couldn't load because there wasn't a BuildContext to
    reference.

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -28,28 +28,17 @@ class LoginScreenState extends State<LoginScreen>
     with TickerProviderStateMixin {
   AnimationController _loginButtonController;
   var animationStatus = 0;
-  var animationConfig;
   @override
   void initState() {
     super.initState();
     _loginButtonController = new AnimationController(
         duration: new Duration(milliseconds: 3000), vsync: this);
-    loadLoginConfig();
   }
 
   @override
   void dispose() {
     _loginButtonController.dispose();
     super.dispose();
-  }
-
-  Future loadLoginConfig() async {
-    DefaultAssetBundle.of(context)
-        .loadString("config/login/animation.json")
-        .then((config) {
-      _log.info('config:\n$config');
-      animationConfig = json.decode(config);
-    });
   }
 
   Future<Null> _playAnimation() async {
@@ -84,59 +73,74 @@ class LoginScreenState extends State<LoginScreen>
   Widget build(BuildContext context) {
     timeDilation = 0.4;
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
-    return (new WillPopScope(
-        onWillPop: _onWillPop,
-        child: new Scaffold(
-          body: new Container(
-              decoration: new BoxDecoration(
-                image: backgroundImage,
-              ),
-              child: new Container(
-                  decoration: new BoxDecoration(
-                      gradient: new LinearGradient(
-                    colors: <Color>[
-                      const Color.fromRGBO(162, 146, 199, 0.8),
-                      const Color.fromRGBO(51, 51, 63, 0.9),
-                    ],
-                    stops: [0.2, 1.0],
-                    begin: const FractionalOffset(0.0, 0.0),
-                    end: const FractionalOffset(0.0, 1.0),
-                  )),
-                  child: new ListView(
-                    padding: const EdgeInsets.all(0.0),
-                    children: <Widget>[
-                      new Stack(
-                        alignment: AlignmentDirectional.bottomCenter,
-                        children: <Widget>[
-                          new Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              LogoWidget(image: logo),
-                              FormContainer(),
-                              SignUp()
-                            ],
-                          ),
-                          animationStatus == 0
-                              ? new Padding(
-                                  padding: const EdgeInsets.only(bottom: 50.0),
-                                  child: new InkWell(
-                                      onTap: () {
-                                        setState(() {
-                                          animationStatus = 1;
-                                        });
-                                        _playAnimation();
-                                      },
-                                      child: SignIn()),
-                                )
-                              : new StaggerAnimation(
-                                  buttonController: _loginButtonController.view,
-                                  signInOnPressed: () {},
-                                  config: animationConfig,
+
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context)
+          .loadString("config/login/animation.json")
+          .then((config) => json.decode(config)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return CircularProgressIndicator();
+        } else {
+          return (new WillPopScope(
+              onWillPop: _onWillPop,
+              child: new Scaffold(
+                body: new Container(
+                    decoration: new BoxDecoration(
+                      image: backgroundImage,
+                    ),
+                    child: new Container(
+                        decoration: new BoxDecoration(
+                            gradient: new LinearGradient(
+                          colors: <Color>[
+                            const Color.fromRGBO(162, 146, 199, 0.8),
+                            const Color.fromRGBO(51, 51, 63, 0.9),
+                          ],
+                          stops: [0.2, 1.0],
+                          begin: const FractionalOffset(0.0, 0.0),
+                          end: const FractionalOffset(0.0, 1.0),
+                        )),
+                        child: new ListView(
+                          padding: const EdgeInsets.all(0.0),
+                          children: <Widget>[
+                            new Stack(
+                              alignment: AlignmentDirectional.bottomCenter,
+                              children: <Widget>[
+                                new Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    LogoWidget(image: logo),
+                                    FormContainer(),
+                                    SignUp()
+                                  ],
                                 ),
-                        ],
-                      ),
-                    ],
-                  ))),
-        )));
+                                animationStatus == 0
+                                    ? new Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 50.0),
+                                        child: new InkWell(
+                                            onTap: () {
+                                              setState(() {
+                                                animationStatus = 1;
+                                              });
+                                              _playAnimation();
+                                            },
+                                            child: SignIn()),
+                                      )
+                                    : new StaggerAnimation(
+                                        buttonController:
+                                            _loginButtonController.view,
+                                        signInOnPressed: () {},
+                                        config: snapshot.data,
+                                      ),
+                              ],
+                            ),
+                          ],
+                        ))),
+              )));
+        }
+      },
+    );
   }
 }
